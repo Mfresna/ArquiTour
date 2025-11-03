c69feb22cb7897515dcc88a3570f4d56e40f7fc4
matias <matiasgfresnadillo@gmail.com>
2025-11-03 17:24:52 -0300

    Login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { PruebaToken } from './prueba-token/prueba-token';
+import { Login } from './pages/login/login';
 
 export const routes: Routes = [
   
+  { path: '', component: Login },
   { path: 'prueba-token', component: PruebaToken }
 ];
