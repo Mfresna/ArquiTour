79e625a6e8db3f9ebe0ba1b4f2efc69671a352b8
agusfuentes04 <afuentes0491@gmail.com>
2025-11-03 00:13:36 -0300

    auth parcial

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,7 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home-component/home-component';
-import { Detalle } from './pages/detalle/detalle';
 import { PruebaToken } from './prueba-token/prueba-token';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'estudios/:id', component: Detalle },
+  
   { path: 'prueba-token', component: PruebaToken }
 ];
